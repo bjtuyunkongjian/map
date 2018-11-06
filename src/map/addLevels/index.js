7c7ccc3ac9f4800820ed39009244d9503f8f9f5c
sl204984 <17798520003@163.com>
2018-11-06 13:51:47 +0800

    merge

--- src/map/addLevels/index.js
+++ src/map/addLevels/index.js
@@ -8,7 +8,6 @@ import layerLv11 from './layer-lv11';
 import layerLv12 from './layer-lv12';
 import layerLv13 from './layer-lv13';
 import layerLv15 from './layer-lv15';
-<<<<<<< HEAD
 import layerLv16 from './layer-lv16';
 import layerLv17 from './layer-lv17';
 import layerLv18 from './layer-lv18';
@@ -18,14 +17,6 @@ import layerLv18 from './layer-lv18';
 // import layerLv22 from './layer-lv22';
 const addLvs = [layerLv8, layerLv9, layerLv11, layerLv12, layerLv13, layerLv15, layerLv16, layerLv17,
   layerLv18,
-//layerLv19,layerLv20,layerLv21,layerLv22];
-=======
-
-const addLvs = [
-  layerLv8, layerLv9,
-  layerLv10, layerLv11,
-  layerLv12, layerLv13, layerLv15
+  //layerLv19,layerLv20,layerLv21,layerLv22
 ];
-
->>>>>>> d3b697e9534a8c96021d0cf013af8a9c4b519edb
 export default addLvs;
