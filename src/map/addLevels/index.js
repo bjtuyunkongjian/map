dc9d04ce7415d62d24f889e81da89973e56aff5b
sl204984 <17798520003@163.com>
2018-11-06 15:18:12 +0800

    merge

--- src/map/addLevels/index.js
+++ src/map/addLevels/index.js
@@ -11,20 +11,11 @@ import layerLv15 from './layer-lv15';
 import layerLv16 from './layer-lv16';
 import layerLv17 from './layer-lv17';
 import layerLv18 from './layer-lv18';
-<<<<<<< HEAD
 import layerLv19 from './layer-lv19';
 import layerLv20 from './layer-lv20';
 import layerLv21 from './layer-lv21';
 import layerLv22 from './layer-lv22';
-const addLvs = [layerLv8, layerLv9, layerLv11, layerLv12, layerLv13, layerLv15, layerLv16, layerLv17, layerLv18, layerLv19, layerLv20, layerLv21, layerLv22];
-=======
-// import layerLv19 from './layer-lv19';
-// import layerLv20 from './layer-lv20';
-// import layerLv21 from './layer-lv21';
-// import layerLv22 from './layer-lv22';
-const addLvs = [layerLv8, layerLv9, layerLv11, layerLv12, layerLv13, layerLv15, layerLv16, layerLv17,
-  layerLv18,
-  //layerLv19,layerLv20,layerLv21,layerLv22
-];
->>>>>>> 7c7ccc3ac9f4800820ed39009244d9503f8f9f5c
+
+const addLvs = [layerLv8, layerLv9, layerLv10, layerLv11, layerLv12, layerLv13, layerLv15, layerLv16, layerLv17, layerLv18, layerLv19, layerLv20, layerLv21, layerLv22];
+
 export default addLvs;
