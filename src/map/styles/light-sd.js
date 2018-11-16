7091ae2543cba90f5906b945111baf0c15f02813
sl204984 <17798520003@163.com>
2018-11-16 13:42:23 +0800

    geojson

--- src/map/styles/light-sd.js
+++ src/map/styles/light-sd.js
@@ -21,190 +21,192 @@ const _ditchVisibleLevel = 14; // 沟和渠道显示等级
 const _maxzoom = 12;
 
 const layers = [{
-  id: 'background', // 背景
-  type: 'background',
-  layout: {},
-  paint: {
-    'background-color': '#f5f5f5'
-  }
-},
-// 面
-{
-  id: 'GHYDPL_7L', // 记录了一些水渠、河沟，水库的面状要素
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'GHYDPL_Merge', // py是面
-  filter: [
-    'any',
-    ['==', 'CLASID', '210200'],
-    ['==', 'CLASID', '230101'],
-    ['==', 'CLASID', '240101'],
-    ['==', 'CLASID', '250100']
-  ],
-  paint: {
-    'fill-color': '#c4daf6',
-    'fill-opacity': 1,
-    'fill-antialias': false
-  }
-},
-{
-  id: 'GHYDPL_OTH', // 记录了一些水渠、河沟，水库的面状要素
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'GHYDPL_Merge', // py是面
-  filter: [
-    'all',
-    ['!=', 'CLASID', '210200'],
-    ['!=', 'CLASID', '230101'],
-    ['!=', 'CLASID', '240101'],
-    ['!=', 'CLASID', '250100']
-  ],
-  minzoom: _ditchVisibleLevel,
-  layout: {},
-  paint: {
-    'fill-color': '#c4daf6',
-    'fill-opacity': 1,
-    'fill-antialias': false
-  }
-},
-// 线
-{
-  id: 'GBOULN', // 记录了各个镇的边界，有名字的记录的是省界和岛屿（name不为空）
-  type: 'line',
-  source: levelConfig.addLv7,
-  'source-layer': 'GBOULN', // LN，line的简写
-  filter: [
-    'any',
-    ['==', 'CLASID', '630201'],
-    ['==', 'CLASID', '630202'],
-    ['==', 'CLASID', '640201'],
-    ['==', 'CLASID', '250100']
-  ],
-  layout: {
-    'line-cap': 'round',
-    'line-join': 'round'
+    id: 'background', // 背景
+    type: 'background',
+    layout: {},
+    paint: {
+      'background-color': '#f5f5f5'
+    }
   },
-  paint: {
-    'line-width': 1,
-    'line-color': '#cdcdcd',
-    // 'line-dasharray': [5, 4]
-  }
-},
-
-{
-  id: 'GRAILN_bg', // 记录了铁路，底层颜色灰色
-  type: 'line',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107', // LN，line的简写
-  layout: {
-    'line-cap': 'round',
-    'line-join': 'round'
+  // 面
+  {
+    id: 'GHYDPL_7L', // 记录了一些水渠、河沟，水库的面状要素
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'GHYDPL_Merge', // py是面
+    filter: [
+      'any',
+      ['==', 'CLASID', '210200'],
+      ['==', 'CLASID', '230101'],
+      ['==', 'CLASID', '240101'],
+      ['==', 'CLASID', '250100']
+    ],
+    paint: {
+      'fill-color': '#c4daf6',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
   },
-  paint: {
-    'line-color': '#B6B3B7',
-    'line-width': 2.4
-  }
-},
-{
-  id: 'GRAILN', // 记录了铁路，间隔白色
-  type: 'line',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107', // LN，line的简写
-  layout: {
-    'line-cap': 'round',
-    'line-join': 'round'
+  {
+    id: 'GHYDPL_OTH', // 记录了一些水渠、河沟，水库的面状要素
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'GHYDPL_Merge', // py是面
+    filter: [
+      'all',
+      ['!=', 'CLASID', '210200'],
+      ['!=', 'CLASID', '230101'],
+      ['!=', 'CLASID', '240101'],
+      ['!=', 'CLASID', '250100']
+    ],
+    minzoom: _ditchVisibleLevel,
+    layout: {},
+    paint: {
+      'fill-color': '#c4daf6',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
   },
-  paint: {
-    'line-color': '#FFFFFF',
-    'line-dasharray': [5, 5],
-    'line-width': 1.6
-  }
-},
-
-{
-  id: 'line-zd-ref', // 做线的基层使用，zadao
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107',
-  layout: {},
-  paint: {
-    'fill-opacity': 0
-  }
-},
-{
-  id: 'line-xdzgd-ref', // 做线的基层使用，县道主干道
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107',
-  layout: {},
-  paint: {
-    'fill-opacity': 0
-  }
-},
-{
-  id: 'line-ref', // 做线的基层使用，国道、省道
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107',
-  layout: {},
-  paint: {
-    'fill-opacity': 0
-  }
-},
-/////////////////////////////
-{
-  id: '3d-ref', // 做线的基层使用，国道、省道
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'SD_GRAILN_1107',
-  layout: {},
-  paint: {
-    'fill-opacity': 0
-  }
-},
-
-///////////////////////////////
-// 点
-
-{
-  id: 'symbol-ref', // 做文字的基层使用
-  type: 'fill',
-  source: levelConfig.addLv7,
-  'source-layer': 'POI_LEVEL_7',
-  layout: {},
-  paint: {
-    'fill-opacity': 0
-  }
-},
-
-
-{
-  id: 'POI_LEVEL7', // POI图层
-  type: 'symbol',
-  source: levelConfig.addLv7,
-  'source-layer': 'POI_LEVEL_7',
-  maxzoom: _maxzoom,
-  layout: {
-    'text-field': '{NAME}',
-    visibility: 'visible',
-    'symbol-placement': 'point',
-    'text-size': 16,
-    'text-padding': 4,
-    'icon-image': 'ic_map_{KIND}',
-    'text-justify': 'left',
-    'text-anchor': 'left',
-    'text-offset': [0.8, 0],
-    'text-font': ['Arial Unicode MS Blod', 'Open Sans Regular'],
-    'text-pitch-alignment': 'viewport',
-    'text-rotation-alignment': 'viewport',
-    'icon-rotation-alignment': 'viewport'
+  // 线
+  {
+    id: 'GBOULN', // 记录了各个镇的边界，有名字的记录的是省界和岛屿（name不为空）
+    type: 'line',
+    source: levelConfig.addLv7,
+    'source-layer': 'GBOULN', // LN，line的简写
+    filter: [
+      'any',
+      ['==', 'CLASID', '630201'],
+      ['==', 'CLASID', '630202'],
+      ['==', 'CLASID', '640201'],
+      ['==', 'CLASID', '250100']
+    ],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-width': 1,
+      'line-color': '#cdcdcd',
+      // 'line-dasharray': [5, 4]
+    }
   },
-  paint: {
-    'text-color': 'rgba(65, 65, 65, 1)',
-    'text-halo-width': 2,
-    'text-halo-color': 'rgba(255, 255, 255, 1)'
+
+  {
+    id: 'GRAILN_bg', // 记录了铁路，底层颜色灰色
+    type: 'line',
+    source: levelConfig.addLv7,
+    'source-layer': 'SD_GRAILN_1107', // LN，line的简写
+    maxzoom: 12,
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-color': '#B6B3B7',
+      'line-width': 2.4
+    }
+  },
+  {
+    id: 'GRAILN', // 记录了铁路，间隔白色
+    type: 'line',
+    source: levelConfig.addLv7,
+    'source-layer': 'SD_GRAILN_1107', // LN，line的简写
+    maxzoom: 12,
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-color': '#FFFFFF',
+      'line-dasharray': [5, 5],
+      'line-width': 1.6
+    }
+  },
+
+  {
+    id: 'line-zd-ref', // 做线的基层使用，zadao
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'empty',
+    layout: {},
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+  {
+    id: 'line-xdzgd-ref', // 做线的基层使用，县道主干道
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'empty',
+    layout: {},
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+  {
+    id: 'line-ref', // 做线的基层使用，国道、省道
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'empty',
+    layout: {},
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+  /////////////////////////////
+  {
+    id: '3d-ref', // 做线的基层使用，国道、省道
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'empty',
+    layout: {},
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+
+  ///////////////////////////////
+  // 点
+
+  {
+    id: 'symbol-ref', // 做文字的基层使用
+    type: 'fill',
+    source: levelConfig.addLv7,
+    'source-layer': 'POI_LEVEL_7',
+    layout: {},
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+
+
+  {
+    id: 'POI_LEVEL7', // POI图层
+    type: 'symbol',
+    source: levelConfig.addLv7,
+    'source-layer': 'POI_LEVEL_7',
+    maxzoom: _maxzoom,
+    layout: {
+      'text-field': '{NAME}',
+      visibility: 'visible',
+      'symbol-placement': 'point',
+      'text-size': 16,
+      'text-padding': 4,
+      'icon-image': 'ic_map_{KIND}',
+      'text-justify': 'left',
+      'text-anchor': 'left',
+      'text-offset': [0.8, 0],
+      'text-font': ['Arial Unicode MS Blod', 'Open Sans Regular'],
+      'text-pitch-alignment': 'viewport',
+      'text-rotation-alignment': 'viewport',
+      'icon-rotation-alignment': 'viewport'
+    },
+    paint: {
+      'text-color': 'rgba(65, 65, 65, 1)',
+      'text-halo-width': 2,
+      'text-halo-color': 'rgba(255, 255, 255, 1)'
+    }
   }
-}
 ];
 
 export default {
--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -1,7 +1,7 @@
 export default {
   host: 'http://116.62.186.152',
   port: '8080',
-  geojsonHost: 'http://localhost:3000/',
+  geojsonHost: 'http://192.168.43.24:3000/',
   geojsonPort: '3000',
   HTTP_TIME_OUT: 5
 }
