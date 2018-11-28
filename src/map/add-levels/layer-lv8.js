5237a80b48bb85d94836e291dd0bd079d52af319
haoyihong <940761990@qq.com>
2018-11-28 11:08:27 +0800

    增加other道路

--- src/map/add-levels/layer-lv8.js
+++ src/map/add-levels/layer-lv8.js
@@ -2,33 +2,32 @@
  * @author sl 2019-01-02
  * 八级是没有图标的
  */
-import { levelConfig } from 'tuyun-config';
+import { levelConfig } from "tuyun-config";
 
 const _visibleLevel = 8;
-const symbolLabelLayerId = 'symbol-ref';
+const symbolLabelLayerId = "symbol-ref";
 const _maxzoom = 10;
 
 const style = {
   visibleLevel: _visibleLevel,
   source: {
     [levelConfig.addLv8]: {
-      type: 'vector',
-      scheme: 'tms',
+      type: "vector",
+      scheme: "tms",
       tiles: [
-        'http://116.62.186.152:8080/geoserver/gwc/service/tms/1.0.0/SDWorkSpace%3ASD_8L@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf'
+        "http://116.62.186.152:8080/geoserver/gwc/service/tms/1.0.0/SDWorkSpace%3ASD_8L@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf"
       ],
       minzoom: _visibleLevel
     }
   },
   layers: [
     {
-      id: 'POI_LEVEL8',
-      type: 'symbol',
+      id: "POI_LEVEL8",
+      type: "symbol",
       source: levelConfig.addLv8,
-      'source-layer': 'POI_LEVEL_8',
+      "source-layer": "POI_LEVEL_8",
       maxzoom: _maxzoom,
       layout: {
-<<<<<<< HEAD
         "text-field": "{NAME}",
         visibility: "visible",
         "symbol-placement": "point",
@@ -42,26 +41,11 @@ const style = {
         "icon-rotation-alignment": "viewport",
         "text-anchor": "center",
         "text-keep-upright": false
-=======
-        'text-field': '{NAME}',
-        visibility: 'visible',
-        'symbol-placement': 'point',
-        'text-size': 13,
-        'icon-text-fit': 'both',
-        'icon-text-fit-padding': [2, 4, 2, 4],
-        'text-justify': 'center',
-        'text-font': ['黑体'],
-        'text-pitch-alignment': 'viewport',
-        'text-rotation-alignment': 'viewport',
-        'icon-rotation-alignment': 'viewport',
-        'text-anchor': 'center',
-        'text-keep-upright': false
->>>>>>> 57cc166f0ea53b5776977c76011684a3a64d039e
       },
       paint: {
-        'text-color': 'rgba(65, 65, 65, 0.9)',
-        'text-halo-width': 2,
-        'text-halo-color': 'rgba(255, 255, 255, 1)'
+        "text-color": "rgba(65, 65, 65, 0.9)",
+        "text-halo-width": 2,
+        "text-halo-color": "rgba(255, 255, 255, 1)"
       },
       labelLayerId: symbolLabelLayerId
     }
--- src/map/add-levels/road.js
+++ src/map/add-levels/road.js
@@ -1,28 +1,30 @@
-import Guodao from './road-guodao';
-import RoadShengdao from './road-shengdao';
-import XianDao from './road-xiandao';
-import Gjl from './road-gjl';
-import Ksl from './road-ksl';
-import Zgd from './road-zgd';
-import ZaDao from './road-zadao';
-import Cgd from './road-cgd';
-import XiangDao from './road-xiangdao';
-import Zx from './road-zx';
+import Guodao from "./road-guodao";
+import RoadShengdao from "./road-shengdao";
+import XianDao from "./road-xiandao";
+import Gjl from "./road-gjl";
+import Ksl from "./road-ksl";
+import Zgd from "./road-zgd";
+import ZaDao from "./road-zadao";
+import Cgd from "./road-cgd";
+import XiangDao from "./road-xiangdao";
+import Zx from "./road-zx";
+import Other from "./road-other";
 
 const _visibleLevel = 7;
 
 const style = {
   visibleLevel: _visibleLevel,
   source: {
-    'road-source': {
-      type: 'vector',
-      scheme: 'tms',
+    "road-source": {
+      type: "vector",
+      scheme: "tms",
       tiles: [
-        'http://116.62.186.152:12808/originMapServer/string?test=200&type=tms&zoom={z}&row={x}&column={y}'
+        "http://116.62.186.152:12808/originMapServer/string?test=200&type=tms&zoom={z}&row={x}&column={y}"
       ]
     }
   },
   layers: [
+    ...Other,
     ...Zx,
     ...XiangDao,
     ...Ksl,
