e5f05d3b804801b8ba60ac6170a5fe23ccf3e074
sl204984 <17798520003@163.com>
2018-11-30 00:14:21 +0800

    配置文字

--- src/map/map-styles/light-sd.js
+++ src/map/map-styles/light-sd.js
@@ -13,215 +13,216 @@
  * 面在最底层，其次是线，其次是点；居民区在绿地上面，绿地在水系面上面
  */
 
-import { levelConfig } from "tuyun-config";
+import { levelConfig } from 'tuyun-config';
+// import { FontColor } from 'tuyun-utils';
 
 const _ditchVisibleLevel = 14; // 沟和渠道显示等级
 const _maxzoom = 12;
 
 const layers = [
   {
-    id: "background", // 背景
-    type: "background",
+    id: 'background', // 背景
+    type: 'background',
     layout: {},
     paint: {
-      "background-color": "#f5f5f5"
+      'background-color': '#f5f5f5'
     }
   },
   // 面
   {
-    id: "GVEGPL", // 记录了绿地
-    type: "fill",
+    id: 'GVEGPL', // 记录了绿地
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "GVEGPL_Merge",
+    'source-layer': 'GVEGPL_Merge',
     layout: {},
     paint: {
-      "fill-color": "#ccebc8",
-      "fill-opacity": 1,
-      "fill-antialias": false
+      'fill-color': '#ccebc8',
+      'fill-opacity': 1,
+      'fill-antialias': false
     },
-    labelLayerId: "GHYDPL_7L"
+    labelLayerId: 'GHYDPL_7L'
   },
   {
-    id: "GHYDPL_7L", // 记录了一些水渠、河沟，水库的面状要素
-    type: "fill",
+    id: 'GHYDPL_7L', // 记录了一些水渠、河沟，水库的面状要素
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "GHYDPL_Merge", // py是面
+    'source-layer': 'GHYDPL_Merge', // py是面
     filter: [
-      "any",
-      ["==", "CLASID", "210200"],
-      ["==", "CLASID", "230101"],
-      ["==", "CLASID", "240101"],
-      ["==", "CLASID", "250100"]
+      'any',
+      ['==', 'CLASID', '210200'],
+      ['==', 'CLASID', '230101'],
+      ['==', 'CLASID', '240101'],
+      ['==', 'CLASID', '250100']
     ],
     paint: {
-      "fill-color": "#b3d7ff",
-      "fill-opacity": 1,
-      "fill-antialias": false
+      'fill-color': '#b3d7ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
     }
   },
   {
-    id: "GHYDPL_OTH", // 记录了一些水渠、河沟，水库的面状要素
-    type: "fill",
+    id: 'GHYDPL_OTH', // 记录了一些水渠、河沟，水库的面状要素
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "GHYDPL_Merge", // py是面
+    'source-layer': 'GHYDPL_Merge', // py是面
     filter: [
-      "all",
-      ["!=", "CLASID", "210200"],
-      ["!=", "CLASID", "230101"],
-      ["!=", "CLASID", "240101"],
-      ["!=", "CLASID", "250100"]
+      'all',
+      ['!=', 'CLASID', '210200'],
+      ['!=', 'CLASID', '230101'],
+      ['!=', 'CLASID', '240101'],
+      ['!=', 'CLASID', '250100']
     ],
     minzoom: _ditchVisibleLevel,
     layout: {},
     paint: {
-      "fill-color": "#b3d7ff",
-      "fill-opacity": 1,
-      "fill-antialias": false
+      'fill-color': '#b3d7ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
     }
   },
   // 线
   {
-    id: "GBOULN", // 记录了各个镇的边界，有名字的记录的是省界和岛屿（name不为空）
-    type: "line",
+    id: 'GBOULN', // 记录了各个镇的边界，有名字的记录的是省界和岛屿（name不为空）
+    type: 'line',
     source: levelConfig.addLv7,
-    "source-layer": "GBOULN", // LN，line的简写
+    'source-layer': 'GBOULN', // LN，line的简写
     filter: [
-      "any",
-      ["==", "CLASID", "630201"],
-      ["==", "CLASID", "630202"],
-      ["==", "CLASID", "640201"],
-      ["==", "CLASID", "250100"]
+      'any',
+      ['==', 'CLASID', '630201'],
+      ['==', 'CLASID', '630202'],
+      ['==', 'CLASID', '640201'],
+      ['==', 'CLASID', '250100']
     ],
     layout: {
-      "line-cap": "round",
-      "line-join": "round"
+      'line-cap': 'round',
+      'line-join': 'round'
     },
     paint: {
-      "line-width": 1,
-      "line-color": "#cdcdcd"
+      'line-width': 1,
+      'line-color': '#cdcdcd'
       // 'line-dasharray': [5, 4]
     }
   },
 
   {
-    id: "GRAILN_bg", // 记录了铁路，底层颜色灰色
-    type: "line",
+    id: 'GRAILN_bg', // 记录了铁路，底层颜色灰色
+    type: 'line',
     source: levelConfig.addLv7,
-    "source-layer": "SD_GRAILN", // LN，line的简写
+    'source-layer': 'SD_GRAILN', // LN，line的简写
     layout: {
-      "line-cap": "round",
-      "line-join": "round"
+      'line-cap': 'round',
+      'line-join': 'round'
     },
     paint: {
-      "line-color": "#B6B3B7",
-      "line-width": 2.4
+      'line-color': '#B6B3B7',
+      'line-width': 2.4
     }
   },
   {
-    id: "GRAILN", // 记录了铁路，间隔白色
-    type: "line",
+    id: 'GRAILN', // 记录了铁路，间隔白色
+    type: 'line',
     source: levelConfig.addLv7,
-    "source-layer": "SD_GRAILN", // LN，line的简写
+    'source-layer': 'SD_GRAILN', // LN，line的简写
     layout: {
-      "line-cap": "round",
-      "line-join": "round"
+      'line-cap': 'round',
+      'line-join': 'round'
     },
     paint: {
-      "line-color": "#FFFFFF",
-      "line-dasharray": [5, 5],
-      "line-width": 1.6
+      'line-color': '#FFFFFF',
+      'line-dasharray': [5, 5],
+      'line-width': 1.6
     }
   },
 
   {
-    id: "line-grailn-ref", // 做线的基层使用，铁路
-    type: "fill",
+    id: 'line-grailn-ref', // 做线的基层使用，铁路
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-bg-ref", // 做线的背景基层使用
-    type: "fill",
+    id: 'line-bg-ref', // 做线的背景基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-lv15-ref", // 做线的基层使用
-    type: "fill",
+    id: 'line-lv15-ref', // 做线的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-ref", // 做线的基层使用
-    type: "fill",
+    id: 'line-ref', // 做线的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-sd-bg-ref", // 做线的背景基层使用
-    type: "fill",
+    id: 'line-sd-bg-ref', // 做线的背景基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-sd-ref", // 做线的基层使用
-    type: "fill",
+    id: 'line-sd-ref', // 做线的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "line-gd-ref", // 做线的基层使用，国道
-    type: "fill",
+    id: 'line-gd-ref', // 做线的基层使用，国道
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   /////////////////////////////
   {
-    id: "jq-ref", // 做街区的基层使用
-    type: "fill",
+    id: 'jq-ref', // 做街区的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
   {
-    id: "3d-ref", // 做三维建筑的基层使用
-    type: "fill",
+    id: '3d-ref', // 做三维建筑的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "empty",
+    'source-layer': 'empty',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
 
@@ -229,88 +230,88 @@ const layers = [
   // 点
 
   {
-    id: "GHYDPL_7L_NAME",
-    type: "symbol",
+    id: 'GHYDPL_7L_NAME',
+    type: 'symbol',
     source: levelConfig.addLv7,
-    "source-layer": "GHYDPL_Merge",
-    filter: ["==", "CLASID", "250100"],
+    'source-layer': 'GHYDPL_Merge',
+    filter: ['==', 'CLASID', '250100'],
     layout: {
-      "text-field": "{NAME}",
-      visibility: "visible",
-      "symbol-placement": "point",
-      "text-size": 14,
-      "icon-text-fit": "both",
-      "icon-text-fit-padding": [2, 4, 2, 4],
-      "text-justify": "center",
-      "text-font": ["黑体"],
-      "text-pitch-alignment": "viewport",
-      "text-rotation-alignment": "viewport",
-      "icon-rotation-alignment": "viewport",
-      "text-anchor": "center",
-      "text-keep-upright": false
+      'text-field': '{NAME}',
+      visibility: 'visible',
+      'symbol-placement': 'point',
+      'text-size': 14,
+      'icon-text-fit': 'both',
+      'icon-text-fit-padding': [2, 4, 2, 4],
+      'text-justify': 'center',
+      'text-font': ['黑体'],
+      'text-pitch-alignment': 'viewport',
+      'text-rotation-alignment': 'viewport',
+      'icon-rotation-alignment': 'viewport',
+      'text-anchor': 'center',
+      'text-keep-upright': false
     },
     paint: {
-      "text-color": "rgba(65, 65, 65, 0.9)",
-      "text-halo-width": 2,
-      "text-halo-color": "rgba(255, 255, 255, 1)"
+      'text-color': 'rgba(65, 65, 65, 0.9)',
+      'text-halo-width': 2,
+      'text-halo-color': 'rgba(255, 255, 255, 1)'
     }
   },
 
   {
-    id: "symbol-ref", // 做文字的基层使用
-    type: "fill",
+    id: 'symbol-ref', // 做文字的基层使用
+    type: 'fill',
     source: levelConfig.addLv7,
-    "source-layer": "POI_LEVEL_7",
+    'source-layer': 'POI_LEVEL_7',
     layout: {},
     paint: {
-      "fill-opacity": 0
+      'fill-opacity': 0
     }
   },
 
   {
-    id: "POI_LEVEL7", // POI图层
-    type: "symbol",
+    id: 'POI_LEVEL7', // POI图层
+    type: 'symbol',
     source: levelConfig.addLv7,
-    "source-layer": "POI_LEVEL_7",
+    'source-layer': 'POI_LEVEL_7',
     maxzoom: _maxzoom,
     layout: {
-      "text-field": "{NAME}",
-      visibility: "visible",
-      "symbol-placement": "point",
-      "text-size": 16,
-      "text-padding": 4,
-      "icon-image": "ic_map_{KIND}",
-      "text-justify": "left",
-      "text-anchor": "left",
-      "text-offset": [0.8, 0],
-      "text-font": ["Arial Unicode MS Blod", "Open Sans Regular"],
-      "text-pitch-alignment": "viewport",
-      "text-rotation-alignment": "viewport",
-      "icon-rotation-alignment": "viewport"
+      'text-field': '{NAME}',
+      visibility: 'visible',
+      'symbol-placement': 'point',
+      'text-size': 16,
+      'text-padding': 4,
+      'icon-image': 'ic_map_{KIND}',
+      'text-justify': 'left',
+      'text-anchor': 'left',
+      'text-offset': [0.8, 0],
+      'text-font': ['Arial Unicode MS Blod', 'Open Sans Regular'],
+      'text-pitch-alignment': 'viewport',
+      'text-rotation-alignment': 'viewport',
+      'icon-rotation-alignment': 'viewport'
     },
     paint: {
-      "text-color": "rgba(65, 65, 65, 1)",
-      "text-halo-width": 2,
-      "text-halo-color": "rgba(255, 255, 255, 1)"
+      'text-color': 'rgba(65, 65, 65, 1)', // ['get', ['get', 'KIND'], ['literal', FontColor]]
+      'text-halo-width': 2,
+      'text-halo-color': 'rgba(255, 255, 255, 1)'
     }
   }
 ];
 
 export default {
   version: 8,
-  name: "Mapbox",
+  name: 'Mapbox',
   metadata: {},
   sources: {
     [levelConfig.addLv7]: {
-      type: "vector",
-      scheme: "tms",
+      type: 'vector',
+      scheme: 'tms',
       tiles: [
-        "http://116.62.186.152:8080/geoserver/gwc/service/tms/1.0.0/SDWorkSpace%3ASD_7L@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf"
+        'http://116.62.186.152:8080/geoserver/gwc/service/tms/1.0.0/SDWorkSpace%3ASD_7L@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf'
       ]
     }
   },
-  sprite: "http://116.62.186.152:12808/sprite/sprite",
-  glyphs: "http://47.97.24.100:8899/fonts/{fontstack}/{range}.pbf",
-  visibility: "public",
+  sprite: 'http://localhost:3000/static/sprite',
+  glyphs: 'http://47.97.24.100:8899/fonts/{fontstack}/{range}.pbf',
+  visibility: 'public',
   layers
 };
--- web_modules/tuyun-utils/index.js
+++ web_modules/tuyun-utils/index.js
@@ -2,7 +2,8 @@ import addLevel from './addLevel';
 import RegRgb from './reg-rgb';
 import CreateUid from './create-uid';
 import IsEmpty from './is-empty';
+import FontColor from './font-color';
 
 import { FetchRequest } from './fetch';
 
-export { addLevel, RegRgb, FetchRequest, CreateUid, IsEmpty };
+export { addLevel, RegRgb, FetchRequest, CreateUid, IsEmpty, FontColor };
