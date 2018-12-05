5fc530769e4e6ed31fdcf740e80a30700c371a7a
haoyihong <940761990@qq.com>
2018-12-05 17:15:40 +0800

    超时更改

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -2,6 +2,6 @@ export default {
   host: 'http://116.62.186.152',
   port: '8080',
   geojsonHost: `http://${window.location.hostname}:3000/`,
-  bffHost: `http://192.168.8.111:8080/`,
-  httpTimeOut: 500
+  bffHost: `http://192.168.8.106:3000/`,
+  httpTimeOut: 5
 };
