d2e101408efd22fcc544660dc98b84d8b5fd4a4f
sl204984 <17798520003@163.com>
2018-12-06 13:57:03 +0800

    注释，删除不需要的host

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -1,8 +1,7 @@
 export default {
-  host: 'http://116.62.186.152:8080/',
   geoserverHost: 'http://192.168.8.104:8080/', // geoserver 地址
-  geojsonHost: `http://${window.location.hostname}:3000/`,
+  geojsonHost: `http://${window.location.hostname}:3000/`, // 提供geojson的后台
   tileHost: 'http://192.168.8.104:12808/', // 自己提供瓦片数据的后台，包括精灵图和文字
-  bffHost: `http://192.168.8.111:8080/`,
+  bffHost: `http://192.168.8.111:8080/`, // bff
   httpTimeOut: 50
 };
