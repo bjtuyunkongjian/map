26705802e13f88cec23d1b5140747fe4887b70e2
sl204984 <17798520003@163.com>
2018-12-06 09:47:17 +0800

    base-config

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -1,7 +1,6 @@
 export default {
-  host: 'http://116.62.186.152',
-  port: '8080',
+  host: 'http://116.62.186.152:8080/',
   geojsonHost: `http://${window.location.hostname}:3000/`,
   bffHost: `http://192.168.8.111:8080/`,
-  httpTimeOut: 5
+  httpTimeOut: 50
 };
