414e3d8a53b4c8a5f020fb984b06d38a6009b7aa
sl204984 <17798520003@163.com>
2018-12-06 13:55:35 +0800

    注释

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -2,7 +2,7 @@ export default {
   host: 'http://116.62.186.152:8080/',
   geoserverHost: 'http://192.168.8.104:8080/', // geoserver 地址
   geojsonHost: `http://${window.location.hostname}:3000/`,
-  tileHost: 'http://192.168.8.104:12808/', // 自己提供瓦片数据的后台
+  tileHost: 'http://192.168.8.104:12808/', // 自己提供瓦片数据的后台，包括精灵图和文字
   bffHost: `http://192.168.8.111:8080/`,
   httpTimeOut: 50
 };
