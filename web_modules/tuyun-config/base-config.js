18a1cfab8c104b2fff3c7c4a449f82c477e69487
sl204984 <17798520003@163.com>
2018-12-07 09:35:54 +0800

    添加注释

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -1,5 +1,5 @@
 export default {
-  geoserverHost: 'http://192.168.8.104:8080/', // geoserver 地址
+  geoserverHost: 'http://192.168.8.104:8080/', // geoserver 地址 阿里云： 116.62.186.152
   geojsonHost: `http://${window.location.hostname}:3000/`, // 提供geojson的后台
   tileHost: 'http://192.168.8.104:12808/', // 自己提供瓦片数据的后台，包括精灵图和文字
   bffHost: `http://192.168.8.111:8080/`, // bff
