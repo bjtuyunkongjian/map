8502b7c232805871fbb3c70ea3f336d3c574ab05
sl204984 <17798520003@163.com>
2018-12-10 21:23:22 +0800

    切换瓦片后台

--- web_modules/tuyun-config/base-config.js
+++ web_modules/tuyun-config/base-config.js
@@ -1,7 +1,7 @@
 export default {
   geoserverHost: 'http://116.62.186.152:8080/', // geoserver 地址 阿里云： 116.62.186.152
   geojsonHost: `http://${window.location.hostname}:3000/`, // 提供geojson的后台
-  tileHost: 'http://192.168.8.109:8080/', // 自己提供瓦片数据的后台 47.110.135.245
+  tileHost: 'http://116.62.186.152:12808/', // 自己提供瓦片数据的后台 47.110.135.245
   bffHost: `http://192.168.8.111:8080/`, // bff
   spriteHost: 'http://47.110.135.245:12808/', // 精灵图端口
   glyphsHost: 'http://47.110.135.245:12808/', // 字体端口
