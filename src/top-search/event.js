82899757c52645b3a16e8217dd3d4aa6be8aef5a
sl204984 <17798520003@163.com>
2018-12-25 17:18:10 +0800

    城市信息

---
+++ src/top-search/event.js
@@ -0,0 +1,5 @@
+import { Emitter } from 'tuyun-utils';
+
+const Event = new Emitter();
+
+export default Event;
--- src/top-search/index.js
+++ src/top-search/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { IoMdShareAlt, IoMdSearch } from 'react-icons/io';
+import CityInfo from './city-info';
+import CityList from './city-list';
 
 export default class TopSearch extends Component {
   render() {
     return (
       <div className="top-search">
+        <CityInfo />
         <input type="text" placeholder="图云搜索" className="search-input" />
         <IoMdShareAlt className="route-icon" />
         <div className="search-btn">
           <IoMdSearch className="search-icon" />
         </div>
+
+        <CityList />
       </div>
     );
   }
