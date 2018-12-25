54e6a98fca447a1fac96867afea67a72f7605c49
haoyihong <940761990@qq.com>
2018-12-25 10:54:59 +0800

    车辆下拉菜单的调整

--- src/left-menu/menu-list/police-car.js
+++ src/left-menu/menu-list/police-car.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Event from './event';
 import { IoIosCar } from 'react-icons/io';
 import MenuItem from './menu-item';
+import { IoIosAddCircleOutline } from 'react-icons/io';
 export default class PoliceCar extends Component {
   state = {
     curMenu: -1,
@@ -17,19 +18,30 @@ export default class PoliceCar extends Component {
     const { curMenu, selectedOpt } = this.state;
     const _selected = curMenu === MenuItem.carOption;
     const _arrow = _selected ? 'arrow-down' : 'arrow-right';
-    const _slide = _selected ? 'menu-dis' : 'menu-apr';
+    const _slide = _selected ? 'menu-apr' : 'menu-dis';
     return (
       <div className="menu-item">
-        <div className="item-label" onClick={`${this._selectTrack}`}>
+        <div className="item-label" onClick={this._selectTrack}>
           <IoIosCar />
           车辆
           <div className={`arrow-box ${_selected ? 'changed' : ''}`}>
-            <span className={`arrow arrow-right ${_arrow}`} />
+            <span className={`arrow ${_arrow}`} />
           </div>
-          <li className={`import-car ${_selected ? '' : 'hidden'}`}>
-            重大安保路线
-          </li>
         </div>
+        <ul
+          className={`track-container ${_selected ? '' : 'hidden'} ${_slide}`}
+        >
+          {options.map((item, index) => (
+            <li
+              className={`track-item ${selectedOpt === index ? 'checked' : ''}`}
+              key={`data_option_${index}`}
+              onClick={e => this._checkTrack(item, index, e)}
+            >
+              {item.icon}
+              {item.name}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -40,4 +52,12 @@ export default class PoliceCar extends Component {
       curMenu === MenuItem.carOption ? -1 : MenuItem.carOption
     );
   };
+  _checkTrack = (item, index, e) => {
+    e.stopPropagation();
+    this.setState({ selectedOpt: index });
+  };
 }
+
+const options = [
+  { value: 0, name: '重大安保路线', icon: <IoIosAddCircleOutline /> }
+];
--- src/left-menu/menu-list/police-data.js
+++ src/left-menu/menu-list/police-data.js
@@ -69,7 +69,7 @@ export default class PoliceData extends Component {
       _bounds._sw.lat,
       _bounds._ne.lng
     ];
-    return;
+    // return;
     const { res } = await FetchPopulation({ points: _points });
 
     const _features = res.map(item => {
