0176a315b1823517f0807084f2624704fe738a61
sl204984 <17798520003@163.com>
2018-12-26 10:07:46 +0800

    修改barprompt的bug

--- web_modules/tuyun-kit/charts/bar-prompt.js
+++ web_modules/tuyun-kit/charts/bar-prompt.js
@@ -14,12 +14,13 @@ export default class BarPrompt extends Component {
     maskLeft: 0,
     promptLeft: 0,
     promptTop: 0,
+    promptBottom: 0,
+    isTop: true, // 判断 prompt 是按照top显示还是按照 bottom 显示
     curData: {}
   };
 
   render() {
-    const { padding = {}, chartTop, data } = this.props;
-    const { promptLeft, promptTop, showPrompt, maskLeft, curData } = this.state;
+    const { padding = {}, chartTop } = this.props;
     const { left = 0, right = 0, top = 0, bottom = 0 } = padding;
     // 计算盒子宽度
     return (
@@ -42,40 +43,63 @@ export default class BarPrompt extends Component {
         onMouseMove={this._onMouseMove}
         onMouseLeave={this._onMouseLeave}
       >
-        {showPrompt ? ( // mask 是否显示
-          <div
-            style={{
-              // 采用内联样式，更好的通过js控制
-              position: 'absolute',
-              left: maskLeft,
-              top: 0,
-              width: `${100 / data.length}%`,
-              height: '100%',
-              backgroundColor: 'rgba(102, 102, 102, 0.4)'
-            }}
-          />
-        ) : null}
-
-        {showPrompt ? ( // prompt 是否显示
-          <div
-            style={{
-              position: 'absolute',
-              left: promptLeft,
-              top: promptTop,
-              padding: 5,
-              width: 100,
-              borderRadius: 4,
-              background: 'rgba(0, 0, 0, 0.65)',
-              color: 'white'
-            }}
-          >
-            <div>{curData.label}</div>
-            <div>数值：{curData.count || 0}</div>
-          </div>
-        ) : null}
+        {this._createMask()}
+        {this._createPrompt()}
       </div>
     );
   }
+
+  _createMask = () => {
+    const { data } = this.props;
+    const { showPrompt, maskLeft } = this.state;
+
+    return showPrompt ? ( // mask 是否显示
+      <div
+        style={{
+          // 采用内联样式，更好的通过js控制
+          position: 'absolute',
+          left: maskLeft,
+          top: 0,
+          width: `${100 / data.length}%`,
+          height: '100%',
+          backgroundColor: 'rgba(102, 102, 102, 0.4)'
+        }}
+      />
+    ) : null;
+  };
+
+  _createPrompt = () => {
+    const {
+      promptLeft,
+      promptTop,
+      promptBottom,
+      isTop,
+      showPrompt,
+      curData
+    } = this.state;
+    const _style = isTop
+      ? { left: promptLeft, top: promptTop }
+      : { left: promptLeft, bottom: promptBottom };
+
+    return showPrompt ? ( // prompt 是否显示
+      <div
+        style={Object.assign(
+          {
+            position: 'absolute',
+            padding: 5,
+            width: 100,
+            borderRadius: 4,
+            background: 'rgba(0, 0, 0, 0.65)',
+            color: 'white'
+          },
+          _style
+        )}
+      >
+        <div>{curData.label}</div>
+        <div>数值：{curData.count || 0}</div>
+      </div>
+    ) : null;
+  };
 
   _onClick = e => {};
 
@@ -108,11 +132,15 @@ export default class BarPrompt extends Component {
       const _maskLeft = _index * _cellWidth + left;
       const _promptLeft = _x < _barPromptW - 100 ? _x + 20 : _barPromptW - 100;
       const _promptTop = _y + 10;
+      const _promptBottom = _barPromptH - _y + 10;
+      const _isTop = _y < _barPromptH / 2; // 判断按 top 显示还是按照 bottom 显示
       this.setState({
         showPrompt: true,
         maskLeft: _maskLeft,
         promptLeft: _promptLeft, // 提示的位置
         promptTop: _promptTop,
+        promptBottom: _promptBottom,
+        isTop: _isTop,
         curData: data[_index]
       });
     }
