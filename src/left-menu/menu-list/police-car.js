ed035e6a34af0d6fa6797ecfd7e9da920c32bcc9
sl204984 <17798520003@163.com>
2018-12-28 16:08:38 +0800

    暂存

--- src/left-menu/menu-list/police-car.js
+++ src/left-menu/menu-list/police-car.js
@@ -10,6 +10,10 @@ export default class PoliceCar extends Component {
     curMenu: -1,
     selectedOpt: ''
   };
+
+  _routeStartF = undefined;
+  _roadIds = undefined;
+
   componentDidMount() {
     this._init();
   }
@@ -47,8 +51,11 @@ export default class PoliceCar extends Component {
   }
 
   _init = () => {
-    Event.on('change:curMenu', curMenu => this.setState({ curMenu })); // 选择当前菜单
-    _MAP_.on('mouseup', this._fetchRoadInfo);
+    Event.on('change:curMenu', curMenu => {
+      this.setState({ curMenu, selectedOpt: '' });
+    }); // 选择当前菜单
+    // return;
+    _MAP_.on('mouseup', this._setStart);
     _MAP_.on('mouseup', 'POLICE_CAR_START', e => {
       console.log('e.features', e.features);
     });
@@ -67,17 +74,18 @@ export default class PoliceCar extends Component {
     this.setState({ selectedOpt: index });
   };
 
-  _fetchRoadInfo = async e => {
+  _setStart = async e => {
+    if (this._routeStartF || _MAP_.getLayer('security_route_start')) return;
     const _bound = _MAP_.getBounds();
-    const _cood = e.lngLat;
-    console.log(e.points);
-    if (_MAP_.getLayer('POLICE_CAR_START')) return;
-    const _features = [
+    // const _coord = e.lngLat;
+    const _coord = { lng: 117.10338726989903, lat: 36.69236306153957 };
+
+    this._routeStartF = [
       {
         type: 'Feature',
         geometry: {
           type: 'Point',
-          coordinates: [_cood.lng, _cood.lat]
+          coordinates: [_coord.lng, _coord.lat]
         },
         properties: {
           title: '点',
@@ -85,29 +93,94 @@ export default class PoliceCar extends Component {
         }
       }
     ];
+    this._drawStartPoint(_coord); // 绘制起始点
+
+    console.log('%c ~~~~~~~~~~~~~~~ 第 1 次请求 ~~~~~~~~~~~~~~~', 'color: red');
+    let { res, err } = await FetchRoadInfo({
+      coord: _coord,
+      bound: _bound,
+      order: 'first'
+    });
+    console.log('_fetchRes', { res, err });
+    if (err || !res) return;
+    this._roadIds = res.ids;
+    let _startPoint; // 起始点
+    const _toSelectPoints = [],
+      _toSelectFeatures = [];
+    for (let item of res.points) {
+      if (item.startPoint) {
+        const _ind = item.userData.indexOf('true');
+        item.userData.splice(_ind, 1);
+        _startPoint = item;
+      } else {
+        _toSelectPoints.push(item);
+        _toSelectFeatures.push({});
+      }
+    }
+    console.log(_startPoint, _toSelectPoints);
+    this._fetch({
+      prev: _startPoint,
+      suff: _toSelectPoints[0],
+      ids: this._roadIds,
+      order: 'firstPlus'
+    });
+  };
+
+  _loopCounts = 1;
+  _fetch = async param => {
+    console.log(
+      `%c ~~~~~~~~~~~~~~~ 第 ${2} 次请求 ~~~~~~~~~~~~~~~`,
+      'color: red'
+    );
+    let { res, err } = await FetchRoadInfo(param);
+    console.log('{ res, err }', { res, err });
+  };
+
+  _drawStartPoint = coord => {
+    // 组织点
+    this._routeStartF = [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [coord.lng, coord.lat]
+        },
+        properties: {
+          title: '点',
+          icon: 'monument'
+        }
+      }
+    ];
+    //
     _MAP_.addLayer({
-      id: 'POLICE_CAR_START',
+      id: 'security_route_start',
       type: 'symbol',
       source: {
         type: 'geojson',
         data: {
           type: 'FeatureCollection',
-          features: _features
+          features: this._routeStartF
         }
       },
       layout: {
-        'text-field': '{title}',
+        'text-field': '',
         visibility: 'visible',
         'symbol-placement': 'point',
-        'text-size': 24,
-        'icon-text-fit': 'both',
-        'text-justify': 'center',
+        'text-size': 16,
+        'text-padding': 4,
+        'icon-image': 'security_route_start',
+        'text-justify': 'left',
+        'text-anchor': 'left',
+        'text-offset': [0.5, 0],
         'text-font': ['黑体'],
         'text-pitch-alignment': 'viewport',
         'text-rotation-alignment': 'viewport',
-        'icon-rotation-alignment': 'viewport',
-        'text-anchor': 'center',
-        'text-keep-upright': false
+        'icon-rotation-alignment': 'viewport'
+      },
+      paint: {
+        'text-color': 'rgba(65, 65, 65, 1)', // ['get', ['get', 'KIND'], ['literal', FontColor]]
+        'text-halo-width': 2,
+        'text-halo-color': 'rgba(255, 255, 255, 1)'
       }
     });
   };
