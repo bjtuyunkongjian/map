b632522e3be9999b3c39e6b0c11f4ddce9954dab
haoyihong <940761990@qq.com>
2019-03-01 17:57:37 +0800

    右侧联动

--- src/linkage-display/test-pie.js
+++ src/linkage-display/test-pie.js
@@ -13,11 +13,17 @@ export default class TestPie extends Component {
           legend={{ text: '人口总数：65' }}
           tooltip="访问来源"
           data={[
-            { value: 435, name: '直接访问' },
-            { value: 310, name: '邮件营销' },
-            { value: 234, name: '联盟广告' },
-            { value: 135, name: '视频广告' },
-            { value: 1548, name: '搜索引擎' }
+            { value: 400, name: '网安' },
+            { value: 310, name: '经侦' },
+            { value: 234, name: '刑警' },
+            { value: 135, name: '护证' },
+            { value: 154, name: '禁毒' },
+            { value: 184, name: '情报' },
+            { value: 130, name: '国保' },
+            { value: 121, name: '反邪教' },
+            { value: 139, name: '反恐' },
+            { value: 128, name: '交警' },
+            { value: 190, name: '泽雨' }
           ]}
         />
       </div>
