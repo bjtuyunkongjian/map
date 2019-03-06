ae2c709f0b7fd7491ed377faee39719e6fe5d49b
haoyihong <940761990@qq.com>
2019-03-06 10:10:15 +0800

    饼状图数据

--- src/linkage-display/test-pie.js
+++ src/linkage-display/test-pie.js
@@ -17,19 +17,6 @@ export default class TestPie extends Component {
           legend={{ text: '人口总数：65' }}
           tooltip="访问来源"
           data={[
-<<<<<<< HEAD
-            { value: 400, name: '网安' },
-            { value: 310, name: '经侦' },
-            { value: 234, name: '刑警' },
-            { value: 135, name: '护证' },
-            { value: 154, name: '禁毒' },
-            { value: 184, name: '情报' },
-            { value: 130, name: '国保' },
-            { value: 121, name: '反邪教' },
-            { value: 139, name: '反恐' },
-            { value: 128, name: '交警' },
-            { value: 190, name: '泽雨' }
-=======
             { value: 435, name: '直接访问' },
             { value: 310, name: '邮件营销' },
             { value: 234, name: '联盟广告' },
@@ -46,7 +33,6 @@ export default class TestPie extends Component {
             { value: 310, name: '邮件营销' },
             { value: 234, name: '联盟广告' },
             { value: 135, name: '视频广告' }
->>>>>>> 79f52061c1a5d112a0384dd44cd8159a37e9e468
           ]}
           selectedIndex={selectedIndex}
           onClick={param => {
