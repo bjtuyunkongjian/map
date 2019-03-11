fa9537ceb37bc1f71bd26493f32301a01066226e
sl204984 <17798520003@163.com>
2019-03-11 15:23:19 +0800

    删除多余文件

--- src/left-menu/menu-list/menu-item.js
+++
@@ -1,9 +0,0 @@
-export default {
-  workContent: 'work-option',
-  dataOption: 'data-option',
-  caseOption: 'case-option',
-  callPoliceOption: 'callPolice-option',
-  cameraOption: 'camera-option',
-  securityRoute: 'car-option',
-  policeForce: 'police-force'
-};
