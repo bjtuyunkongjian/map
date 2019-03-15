e594a2f0a0030b7feae745bcee67cfd9e1f4913c
sl204984 <17798520003@163.com>
2019-03-15 17:58:13 +0800

    重点人口详情

--- src/keypop-detail/index.js
+++ src/keypop-detail/index.js
@@ -4,11 +4,35 @@
  */
 
 import React, { Component } from 'react';
+import { Event as GlobalEvent } from 'tuyun-utils';
 
 export default class KeyPopDetail extends Component {
-  state = {};
+  state = {
+    visible: true,
+    selectedIndex: -1
+  };
 
   render() {
-    return <div className="keypop-detail">aaa</div>;
+    const { visible } = this.state;
+    if (!visible) return null;
+    return (
+      <div className="keypop-detail">
+        {[1, 2, 3].map((item, index) => {
+          return (
+            <div
+              key={`menu_item_${index}`}
+              className="menu-item"
+              onClick={this._selectMenu}
+            >
+              标绘
+            </div>
+          );
+        })}
+      </div>
+    );
   }
+
+  _selectMenu = e => {
+    e.stopPropagation();
+  };
 }
