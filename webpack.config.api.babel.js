70fd0155ac7b83f11cb399280cc002ea297a73db
sl204984 <17798520003@163.com>
2019-07-09 23:54:13 +0800

    git

--- webpack.config.api.babel.js
+++ webpack.config.api.babel.js
@@ -1,9 +1,13 @@
 import Path from 'path';
+import PackageJson from './package.json';
 
 export default {
   mode: 'development',
   entry: __dirname + '/src/map/index.api.js',
-  output: { path: __dirname + '/build-api' },
+  output: {
+    path: __dirname + '/build-api',
+    filename: `ty-map.${PackageJson.version}.js` // [name].[hash].bundle.js
+  },
   module: {
     rules: [
       {
