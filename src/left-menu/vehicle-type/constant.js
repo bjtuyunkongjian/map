13304a28b6184e1df961e28c6d8c3f2071a51bf4
sl204984 <17798520003@163.com>
2019-08-06 14:42:21 +0800

    git

--- src/left-menu/vehicle-type/constant.js
+++ src/left-menu/vehicle-type/constant.js
@@ -4,6 +4,7 @@ export const TypeOptions = [
     name: '省内长途',
     hsl: [315, 26, 48],
     rgb: [155, 92, 139],
+    rgbHex: '9b5c8b',
     type: 3
   },
   {
@@ -11,6 +12,7 @@ export const TypeOptions = [
     name: '跨省长途',
     hsl: [208, 59, 51],
     rgb: [56, 134, 204],
+    rgbHex: '3886cc',
     type: 1
   },
   {
@@ -18,6 +20,7 @@ export const TypeOptions = [
     name: '危险车辆',
     hsl: [0, 100, 50],
     rgb: [255, 0, 0],
+    rgbHex: 'ff0000',
     type: 2
   }
 ];
