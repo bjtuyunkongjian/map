3a803cc1bf59c226ce4afd3dcedce6db8ac1edb4
sl204984 <17798520003@163.com>
2019-09-04 02:48:18 +0800

    git

--- src/map/api.index.js
+++ src/map/api.index.js
@@ -7,24 +7,61 @@ import { AddLevel } from 'tuyun-utils/index.base.js';
 const mapArr = [];
 
 class TyMap {
-  constructor(container, options = { hash: true }) {
-    const { hash } = options;
+  constructor(container, options = {}) {
+    const {
+      hash = true,
+      center = [117.0856, 36.6754],
+      zoom = 11,
+      pitch = 0,
+      bearing = 0
+    } = options;
     const map = new mapboxgl.Map({
-      hash: hash || true,
-      container: container,
       style: BaseStyle,
-      showTileBoundaries: true,
-      center: [117.0856, 36.6754],
-      zoom: 11,
-      // pitch: 60,
-      // bearing: -13.6,
+      container: container,
+      hash,
+      center,
+      zoom,
+      pitch,
+      bearing,
       minZoom: 7,
       maxZoom: 20,
       localIdeographFontFamily: '黑体'
     });
+
     this.mapIndex = mapArr.length;
     mapArr.push(map);
   }
+
+  _addSourceFunc = () => {
+    for (let item of AddLevels) {
+      AddLevel(this.map, item);
+    }
+  };
+
+  resize = () => mapArr[this.mapIndex].resize();
+
+  getBounds = () => mapArr[this.mapIndex].getBounds();
+
+  setMaxBounds = bounds => mapArr[this.mapIndex].setMaxBounds(bounds);
+  getMaxBounds = () => mapArr[this.mapIndex].getMaxBounds();
+
+  setMinZoom = zoomLevel => mapArr[this.mapIndex].setMinZoom(zoomLevel);
+  getMinZoom = () => mapArr[this.mapIndex].getMinZoom();
+
+  setMaxZoom = () => mapArr[this.mapIndex].setMaxZoom(zoomLevel);
+  getMaxZoom = () => mapArr[this.mapIndex].getMaxZoom();
+
+  isZooming = () => mapArr[this.mapIndex].isZooming();
+  isRotating = () => mapArr[this.mapIndex].isRotating();
+  isMoving = () => mapArr[this.mapIndex].isMoving();
+
+  queryRenderedFeatures = () => mapArr[this.mapIndex].queryRenderedFeatures();
+  querySourceFeatures = id => mapArr[this.mapIndex].querySourceFeatures(id);
+
+  getLayer = layerId => mapArr[this.mapIndex].getLayer(layerId);
+
+  // 辅助计算
+  unproject = point => mapArr[this.mapIndex].unproject(point);
 }
 
-window.__TyMap__ = TyMap;
+window.TyMap = TyMap;
