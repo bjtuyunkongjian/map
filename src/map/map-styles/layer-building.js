8f76dff781ba46aeb3952e005182a01cb2e086e9
Mac <mac@MacdeMacBook-Pro.local>
2019-11-16 00:19:23 +0800

    12-15级地图配置

--- src/map/map-styles/layer-building.js
+++ src/map/map-styles/layer-building.js
@@ -1,1 +1,49 @@
-export default [];
+import { LevelConfig } from 'tuyun-config';
+
+// 3d 普通建筑颜色和透明度
+const gresplOpacity = 0.5;
+const gresplColor = 'rgb(255, 255, 255)';
+const fillExtrusionHeight = 16;
+
+export default [
+  {
+    id: '15_HOUSE', //15级房屋
+    source: LevelConfig.addLv7,
+    'source-layer': 'RES_PY',
+    type: 'fill-extrusion',
+    paint: {
+      'fill-extrusion-color': gresplColor,
+      'fill-extrusion-height': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        fillExtrusionHeight,
+        0,
+        fillExtrusionHeight + 0.55,
+        ['*', ['+', ['get', 'FLOOR'], 1], 3]
+      ],
+      'fill-extrusion-base': 0,
+      'fill-extrusion-opacity': gresplOpacity
+    }
+  },
+  {
+    id: '15_BUILDING', //15级居民地
+    source: LevelConfig.addLv7,
+    'source-layer': 'RES_PY1',
+    type: 'fill-extrusion',
+    paint: {
+      'fill-extrusion-color': gresplColor,
+      'fill-extrusion-height': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        fillExtrusionHeight,
+        0,
+        fillExtrusionHeight + 0.55,
+        ['*', ['+', ['get', 'FLOOR'], 1], 3]
+      ],
+      'fill-extrusion-base': 0,
+      'fill-extrusion-opacity': gresplOpacity
+    }
+  }
+];
--- src/map/map-styles/layer-river.js
+++ src/map/map-styles/layer-river.js
@@ -8,7 +8,51 @@ export default [
     id: 'HAIYU', // 记录海域，河流的面状要素
     type: 'fill',
     source: LevelConfig.addLv7,
-    'source-layer': 'HaiYu', // py是面
+    'source-layer': 'HaiYu',
+    paint: {
+      'fill-color': '#b3d8ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
+  },
+  {
+    id: 'GQ_LV15', // 沟渠
+    type: 'fill',
+    source: LevelConfig.addLv7,
+    'source-layer': 'GQ',
+    paint: {
+      'fill-color': '#b3d8ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
+  },
+  {
+    id: 'HUCHI_LV15', // 湖泊水池
+    type: 'fill',
+    source: LevelConfig.addLv7,
+    'source-layer': 'Hu-Chi',
+    paint: {
+      'fill-color': '#b3d8ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
+  },
+  {
+    id: 'SKCT_LV15', // 水库池塘
+    type: 'fill',
+    source: LevelConfig.addLv7,
+    'source-layer': 'SK-CT',
+    paint: {
+      'fill-color': '#b3d8ff',
+      'fill-opacity': 1,
+      'fill-antialias': false
+    }
+  },
+  {
+    id: 'ZQ_LV15', // 支渠
+    type: 'fill',
+    source: LevelConfig.addLv7,
+    'source-layer': 'Zhiqu',
     paint: {
       'fill-color': '#b3d8ff',
       'fill-opacity': 1,
