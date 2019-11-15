ff3e97e31ff4996cce17938b91673782ad22fe7e
Mac <mac@MacdeMacBook-Pro.local>
2019-11-15 23:25:29 +0800

    11级POI

--- src/map/map-styles/layer-poi.js
+++ src/map/map-styles/layer-poi.js
@@ -304,6 +304,56 @@ const PoiLayer = [
       'text-halo-width': 2,
       'text-halo-color': 'rgba(255, 255, 255, 1)'
     }
+  },
+  {
+    id: 'LAYER11_LXD', //11级旅游景点
+    type: 'symbol',
+    source: LevelConfig.addLv7,
+    'source-layer': 'LXD11',
+    layout: {
+      'text-field': '{Name}',
+      'symbol-placement': 'point',
+      'text-size': 14,
+      'icon-text-fit': 'both',
+      'icon-text-fit-padding': [2, 4, 2, 4],
+      'text-justify': 'center',
+      'text-font': ['黑体'],
+      'text-pitch-alignment': 'viewport',
+      'text-rotation-alignment': 'viewport',
+      'icon-rotation-alignment': 'viewport',
+      'text-anchor': 'center',
+      'text-keep-upright': false
+    },
+    paint: {
+      'text-color': 'rgba(65, 65, 65, 0.9)',
+      'text-halo-width': 2,
+      'text-halo-color': 'rgba(255, 255, 255, 1)'
+    }
+  },
+  {
+    id: 'LAYER11_DZJG', //11级党政机关
+    type: 'symbol',
+    source: LevelConfig.addLv7,
+    'source-layer': 'DZJG11',
+    layout: {
+      'text-field': '{Name}',
+      'symbol-placement': 'point',
+      'text-size': 14,
+      'icon-text-fit': 'both',
+      'icon-text-fit-padding': [2, 4, 2, 4],
+      'text-justify': 'center',
+      'text-font': ['黑体'],
+      'text-pitch-alignment': 'viewport',
+      'text-rotation-alignment': 'viewport',
+      'icon-rotation-alignment': 'viewport',
+      'text-anchor': 'center',
+      'text-keep-upright': false
+    },
+    paint: {
+      'text-color': 'rgba(65, 65, 65, 0.9)',
+      'text-halo-width': 2,
+      'text-halo-color': 'rgba(255, 255, 255, 1)'
+    }
   }
 ];
 
