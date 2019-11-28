ef4c0b95b2b696dd1d1bd563640695050ab54a8a
sl204984 <17798520003@163.com>
2019-11-29 01:21:05 +0800

    git

--- src/map-hz/map-styles/road/ci-gan-dao.js
+++ src/map-hz/map-styles/road/ci-gan-dao.js
@@ -1,5 +1,5 @@
 import { MapSource } from '../constant';
-import { Layout } from './common';
+import { Layout, PaintCiGanDao } from './common';
 
 const CiGanDao = [
   {
@@ -8,30 +8,7 @@ const CiGanDao = [
     source: MapSource,
     'source-layer': 'CGD',
     layout: Layout,
-    paint: {
-      'line-width': {
-        base: 2,
-        stops: [
-          [7, 3],
-          [8, 2],
-          [9, 3],
-          [10, 4],
-          [11, 4],
-          [12, 3],
-          [13, 5],
-          [14, 6],
-          [15, 10],
-          [16, 10],
-          [17, 12],
-          [18, 14],
-          [19, 14],
-          [20, 22],
-          [21, 24],
-          [22, 26]
-        ]
-      },
-      'line-color': '#d8d8d8'
-    }
+    paint: PaintCiGanDao.bgRoad
   },
   {
     id: 'cgd',
@@ -39,30 +16,7 @@ const CiGanDao = [
     source: MapSource,
     'source-layer': 'CGD',
     layout: Layout,
-    paint: {
-      'line-width': {
-        base: 2,
-        stops: [
-          [7, 2],
-          [8, 1],
-          [9, 2],
-          [10, 3],
-          [11, 3],
-          [12, 3],
-          [13, 4],
-          [14, 5],
-          [15, 7],
-          [16, 7],
-          [17, 9],
-          [18, 11],
-          [19, 11],
-          [20, 19],
-          [21, 22],
-          [22, 24]
-        ]
-      },
-      'line-color': '#fff'
-    }
+    paint: PaintCiGanDao.road
   }
 ];
 
