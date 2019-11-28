a88a1930f3f3828f6a070a6d271e7c66614bda17
sl204984 <17798520003@163.com>
2019-11-28 23:17:43 +0800

    git

--- src/map-hz/map-styles/light-sd.js
+++ src/map-hz/map-styles/light-sd.js
@@ -1,16 +1,8 @@
 /**
  * @author sl
- * 更换服务器：style.sources.tiles[0] 更换内容
- * 以山东省数据为例
- * host:port/geoserver/gwc/service/tms/1.0.0/SDWorkSpace%3ASD_7L@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf
- * 1. ip + host
- * 2. SDWorkSpace ---> 对应的空间名称
- * 3. 3ASD_7L ----> 对应的图层名称
- * 4. z ----> 缩放等级
- * 5. x, y ----> 对应的瓦片
- *
  * 显示规则：
- * 面在最底层，其次是线，其次是点；居民区在绿地上面，绿地在水系面上面
+ * 面在最底层，其次是线，其次是点；
+ * 水系 -> 绿地 -> 道路 -> 道路名称 -> 建筑物 -> POI
  */
 
 import { LevelConfig } from 'tuyun-config';
---
+++ src/map-hz/map-styles/road/teilu.js
@@ -0,0 +1,42 @@
+const TieLu = [
+  {
+    id: 'road-ref', // 做线的基层使用，铁路
+    type: 'fill',
+    source: LevelConfig.addLv7,
+    'source-layer': 'empty',
+    paint: {
+      'fill-opacity': 0
+    }
+  },
+  {
+    id: 'tielu_bg', // 记录了铁路，底层颜色灰色
+    type: 'line',
+    source: LevelConfig.addLv7,
+    'source-layer': 'TRA_LRR_LN', // LN，line的简写
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-color': '#B6B3B7',
+      'line-width': 2.4
+    }
+  },
+  {
+    id: 'tielu', // 记录了铁路，间隔白色
+    type: 'line',
+    source: LevelConfig.addLv7,
+    'source-layer': 'TRA_LRR_LN', // LN，line的简写
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-color': '#FFFFFF',
+      'line-dasharray': [5, 5],
+      'line-width': 1.6
+    }
+  }
+];
+
+export default TieLu;
