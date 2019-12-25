92a4eb123a35705b21460eeefa59b52a420e2364
sl204984 <17798520003@163.com>
2019-12-25 09:40:22 +0800

    git

--- src/tab-view/building-tab/chart-info.js
+++
@@ -1,7 +0,0 @@
-const ChartName = {
-  buildingBar: 'building-bar'
-};
-
-const BuildingLayerId = 'LK_BUILDING_LAYER';
-
-export { ChartName, BuildingLayerId };
--- src/tab-view/unit-tab/chart-info.js
+++
@@ -1,9 +0,0 @@
-const ChartName = {
-  unitBar: 'unit-bar',
-  specialUnit: 'special-unit',
-  protectUnit: 'protect-unit'
-};
-
-const UnitLayerId = 'LK_UNIT_LAYER';
-
-export { ChartName, UnitLayerId };
