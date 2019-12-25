2d998b388115bd0ca056ceb5df71cf9fe8476a64
sl204984 <17798520003@163.com>
2019-12-25 08:08:38 +0800

    git

--- src/screen/right-chart.js
+++ src/screen/right-chart.js
@@ -6,22 +6,22 @@ export default class RightChart extends Component {
     return (
       <div className="right-chart">
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 人口柱状图 */}</div>
         </div>
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 人口密度图 */}</div>
         </div>
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 单位柱状图 */}</div>
         </div>
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 单位饼状图 */}</div>
         </div>
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 案件折线图 */}</div>
         </div>
         <div className="chart-container">
-          <div className="chart-item">{/* 此处添加图标 */}</div>
+          <div className="chart-item">{/* 警情折线图 */}</div>
         </div>
       </div>
     );
--- src/screen/top-nav.js
+++ src/screen/top-nav.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 
 export default class TopNav extends Component {
-  state = {};
+  state = {
+    year: '',
+    month: '',
+    date: '',
+    hour: '',
+    minute: '',
+    second: ''
+  };
+
+  componentDidMount = () => {
+    this._createTime();
+    this._updateTime();
+  };
+
   render() {
-    return <div className="top-nav">警力一张图可视化平台</div>;
+    const { year, month, date, hour, minute, second } = this.state;
+    return (
+      <div className="top-nav">
+        <div className="time-container">{`${year}年${month}月${date}日 ${hour}:${minute}:${second}`}</div>
+        <div className="title-container">
+          <span className="title-text">警力一张图可视化平台</span>
+        </div>
+        <div>北京图云空间信息股份有限公司</div>
+      </div>
+    );
   }
+
+  _createTime = () => {
+    const date = new Date();
+    this.setState({
+      year: date.getFullYear(),
+      month: format(date.getMonth() + 1),
+      date: format(date.getDate()),
+      hour: format(date.getHours()),
+      minute: format(date.getMinutes()),
+      second: format(date.getSeconds())
+    });
+  };
+
+  _updateTime() {
+    setInterval(() => this._createTime(), 1000);
+  }
+}
+
+function format(num) {
+  const numStr = num.toString();
+  return numStr.length > 1 ? numStr : '0' + numStr;
 }
