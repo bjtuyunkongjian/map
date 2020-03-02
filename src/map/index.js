2ded0306f24cab47a59aaac94ce346c7f519b8cb
sl204984 <17798520003@163.com>
2020-03-02 08:58:02 +0800

    git

--- src/map/index.js
+++ src/map/index.js
@@ -4,11 +4,11 @@
  */
 
 import mapboxgl from 'mapbox-gl';
-import { AddLevel } from 'tuyun-utils';
+// import { AddLevel } from 'tuyun-utils';
 import React, { Component } from 'react';
 
 import BaseStyle from './map-styles/light-sd';
-import AddLevels from './add-levels';
+// import AddLevels from './add-levels';
 // import CustomLayer from './police';
 
 export default class MapBoxDemo extends Component {
@@ -44,52 +44,17 @@ export default class MapBoxDemo extends Component {
       localIdeographFontFamily: '黑体',
       preserveDrawingBuffer: true
     });
-    // 点击地图在控制台打出经纬度 15/36.6866/117.05608
-    // this.map.on('mouseup', async e => {
-    //   const _bounds = _MAP_.getBounds();
-    //   const _xDiff = _bounds._ne.lng - _bounds._sw.lng;
-    //   const _yDiff = _bounds._ne.lat - _bounds._sw.lat;
-    //   console.log(
-    //     'e.lngLat',
-    //     e.lngLat,
-    //     '屏幕范围精度差：',
-    //     _xDiff,
-    //     '屏幕范围纬度差：',
-    //     _yDiff
-    //   );
+    this.map.on('style.load', () => {
+      // this._addSourceFunc(); // 增加图层组
+    });
+    // .on('zoomend', () => {
+    //   this._addSourceFunc();
     // });
-    // this.map.on('click', e => {
-    //   console.log(e.lngLat);
-    //   // console.log(this.map.queryRenderedFeatures(e.point));
-    // });
-    this.map
-      .on('style.load', () => {
-        this.map.loadImage(
-          'http://47.110.135.245:12808/static/1.png',
-          (error, image) => {
-            if (error) throw error;
-            this.map.addImage('catcatcat', image);
-          }
-        );
-        this._addSourceFunc(); // 增加图层组
-        // for (let i = 0; i < 1; i++) {
-        //   this.map.addLayer(
-        //     new CustomLayer({
-        //       id: '3d-model-policeman-obj-' + i,
-        //       height: 21,
-        //       center: [117.03147871 + i / 11100, 36.67556967 + i / 11100]
-        //     })
-        //   );
-        // }
-      })
-      .on('zoomend', () => {
-        this._addSourceFunc();
-      });
   };
 
-  _addSourceFunc = () => {
-    for (let item of AddLevels) {
-      AddLevel(this.map, item);
-    }
-  };
+  // _addSourceFunc = () => {
+  //   for (let item of AddLevels) {
+  //     AddLevel(this.map, item);
+  //   }
+  // };
 }
--- src/mode-vehicle/event.js
+++
@@ -1,8 +0,0 @@
-import { Emitter } from 'tuyun-utils';
-
-export default new Emitter(); // Event
-
-export const EventName = {
-  changeMode: 'change:mode',
-  toggleOptions: 'toggle:options'
-};
