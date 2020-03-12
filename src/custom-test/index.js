e43e7b6eaa615396b25ace3db8521432148e2d99
sl204984 <17798520003@163.com>
2020-03-12 22:29:12 +0800

    git

--- src/custom-test/index.js
+++ src/custom-test/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { FetchRequest } from 'tuyun-utils';
 import CustomLayer from './custom-layer';
-import Lnglat from './lnglat';
 
 export default class index extends Component {
   state = {
-    scale: 5e-8
+    scale: 2e-8
   };
 
   _shouldRemove = false;
+  _buildingArr = [];
 
   componentDidMount = () => this._init();
 
@@ -28,49 +29,88 @@ export default class index extends Component {
   }
 
   _init = () => {
-    _MAP_.on('style.load', () => {
-      this._loadModels();
-    });
-    // .on('moveend', () => {
-    //   this._loadModels(true);
-    // });
+    _MAP_
+      .on('style.load', () => {
+        this._loadModels();
+      })
+      .on('moveend', () => {
+        this._loadModels();
+      });
   };
 
-  _loadModels = () => {
-    const { scale } = this.state;
+  _loadModels = async () => {
+    const _zoom = _MAP_.getZoom();
+    // 小于 16 级，删除所有 id
+    if (_zoom < 16) {
+      for (let item of this._buildingArr) {
+        _MAP_.removeLayer('model-' + item.id);
+      }
+      return;
+    }
+    // 获取屏幕范围
     const _bounds = _MAP_.getBounds();
-    const buildings = Lnglat.filter((item, index) => {
+    // 获取新的建筑物
+    const _url = `mod/getPointKey?minX=${_bounds._sw.lng}&maxX=${_bounds._ne.lng}&minY=${_bounds._sw.lat}&maxY=${_bounds._ne.lat}`;
+    const { res, err } = await FetchRequest({ url: _url });
+    if (err || !res) return console.error('没获取到返回数据');
+    // 等请求结束删除不在屏幕范围之内的建筑物
+    for (let item of this._buildingArr) {
       const { lnglat } = item;
-      if (
+      !(
         lnglat[0] < _bounds._ne.lng &&
         lnglat[0] > _bounds._sw.lng &&
         lnglat[1] < _bounds._ne.lat &&
         lnglat[1] > _bounds._sw.lat
-      )
-        return true;
-    });
-
-    for (let item of buildings) {
-      this._shouldRemove && _MAP_.removeLayer('model-' + item.id);
-      const { lnglat } = item;
-      _MAP_.addLayer(
-        new CustomLayer(
-          lnglat[0],
-          lnglat[1],
-          0,
-          'model-' + item.id,
-          `http://47.110.135.245:12808/static/test/${item.id}.gltf`,
-          scale
-        ),
-        'GHYDPL_7L_NAME'
-      );
+      ) &&
+        _MAP_.getLayer('model-' + item.id) &&
+        _MAP_.removeLayer('model-' + item.id);
     }
-    if (!this._shouldRemove) this._shouldRemove = true;
+    const { scale } = this.state;
+
+    for (let item of res) {
+      if (!_MAP_.getLayer('model-' + item.id)) {
+        const { lnglat } = item;
+        _MAP_.addLayer(
+          new CustomLayer(
+            lnglat[0],
+            lnglat[1],
+            0,
+            'model-' + item.id,
+            `http://47.110.135.245:12808/static/test/${item.id}.gltf`,
+            scale
+          ),
+          'GHYDPL_7L_NAME'
+        );
+      }
+    }
+
+    // 赋予新值
+    this._buildingArr = res;
   };
 
   _changeScale = async w => {
     const { scale } = this.state;
     this.setState({ scale: w * scale });
-    this._loadModels();
+
+    for (let item of this._buildingArr) {
+      // 删除对应模型
+      _MAP_.getLayer('model-' + item.id) &&
+        _MAP_.removeLayer('model-' + item.id);
+      // 重绘
+      if (!_MAP_.getLayer('model-' + item.id)) {
+        const { lnglat } = item;
+        _MAP_.addLayer(
+          new CustomLayer(
+            lnglat[0],
+            lnglat[1],
+            0,
+            'model-' + item.id,
+            `http://47.110.135.245:12808/static/test/${item.id}.gltf`,
+            w * scale
+          ),
+          'GHYDPL_7L_NAME'
+        );
+      }
+    }
   };
 }
