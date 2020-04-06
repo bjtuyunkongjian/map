3c03ca0de5dc54f5e5e5ae0141788672b020f994
sl204984 <17798520003@163.com>
2020-04-07 00:52:36 +0800

    git

--- src/map-sd/config.js
+++ src/map-sd/config.js
@@ -1,9 +1,9 @@
 export default {
-  geoserverHost: `http://47.97.230.212:8080/`, // geoserver 地址 阿里云： 116.62.186.152 10.49.6.62:8080
-  tileHost: `http://47.97.230.212:12808/`, // 自己提供瓦片数据的后台 47.110.135.245
+  geoserverHost: 'http://47.110.135.245:8686/', // geoserver 地址 阿里云： 116.62.186.152 10.49.6.62:8080
+  tileHost: 'http://116.62.186.152:12808/', // 自己提供瓦片数据的后台 47.110.135.245
   spriteHost: `http://47.97.230.212:8082/`, // 精灵图端口
   imageHost: `http://47.97.230.212:8082/`, // 图片地址
   glyphsHost: `http://47.97.230.212:8082/`, // 字体端口
   apiHost: 'http://47.97.230.212:8082/', // api development 环境对应的 ip
-  httpTimeOut: 5000
+  httpTimeOut: 5000,
 };
