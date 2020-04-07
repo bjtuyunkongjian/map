d5baa9144cf4f491e1114afe160d9678da73a8b6
sl204984 <17798520003@163.com>
2020-04-07 15:23:22 +0800

    git

--- src/map/map-styles/light-sd.js
+++ src/map/map-styles/light-sd.js
@@ -6,9 +6,9 @@ const layers = [
   {
     id: 'simple-tiles',
     type: 'raster',
-    source: 'raster-tiles'
+    source: 'raster-tiles',
   },
-  ...Guodao
+  ...Guodao,
 ];
 
 export default {
@@ -19,18 +19,19 @@ export default {
     'raster-tiles': {
       type: 'raster',
       tiles: [`${BaseConfig.wmsHost}get-tiles/dev?z={z}&x={x}&y={y}`],
-      tileSize: 256
+      tileSize: 256,
     },
     'bff-tile-source': {
       type: 'vector',
       scheme: 'tms',
       tiles: [
-        `${BaseConfig.tileHost}originMapServer/string?test=200&type=tms&zoom={z}&row={x}&column={y}`
-      ]
-    }
+        `${BaseConfig.tileHost}originMapServer/string?test=200&type=tms&zoom={z}&row={x}&column={y}`,
+      ],
+    },
   },
   sprite: `${BaseConfig.spriteHost}sprite/sprite`,
   glyphs: `${BaseConfig.glyphsHost}font/{fontstack}/{range}.pbf`, // http://localhost:3000/static/fonts
   visibility: 'public',
-  layers
+  layers,
 };
+// http://124.128.48.215/tileservice/SdRasterPubMap2018?layer=c&style=c&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=18&TileCol=217325&TileRow=38982
