6fc63c6931f241b3cf11b604716aad7e0147464a
sl204984 <17798520003@163.com>
2020-04-09 06:15:10 +0800

    git

---
+++ src/map-sd-1/map-styles-calm/layer-grass.js
@@ -0,0 +1,15 @@
+import { MapSource } from './constant';
+
+export default [
+  {
+    id: 'grass-ref', // 记录了绿地
+    type: 'fill',
+    source: MapSource,
+    'source-layer': 'GVEGPL_Merge',
+    paint: {
+      'fill-color': '#ccd6d7',
+      'fill-opacity': 1,
+      'fill-antialias': false,
+    },
+  },
+];
