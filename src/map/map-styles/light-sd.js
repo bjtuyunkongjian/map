a73d6b72ab5a66f53b49042edce81bd96833b292
sl204984 <17798520003@163.com>
2020-04-21 10:01:24 +0800

    git

--- src/map/map-styles/light-sd.js
+++ src/map/map-styles/light-sd.js
@@ -19,12 +19,12 @@ export default {
     'raster-tiles': {
       type: 'raster',
       tiles: [
-        'https://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=3991cefea7f40e49e48f26915530f3c8',
+        // 'https://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=3991cefea7f40e49e48f26915530f3c8',
         // './static/lvdi.png',
         // '
         // 'http://192.168.251.67:6080/arcgis/rest/services/test1/sdxy1/MapServer/tile/{z}/{y}/{x}/lvdi.png',
         // 'http://localhost:8083/get-tiles/dev?z={z}&x={x}&y={y}',
-        // 'http://192.168.251.11:8888/direct?z={z}&x={x}&y={y}',
+        'http://192.168.251.11:8888/direct?z={z}&x={x}&y={y}',
         // 'http://124.128.48.215/tileservice/spot2006?layer=c&style=c&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix={z}&TileCol={y}&TileRow={x}',
       ],
       tileSize: 256,
