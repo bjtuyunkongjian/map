73d564f457f3427b9bc3116505091e892d064506
sl204984 <17798520003@163.com>
2020-04-21 14:49:45 +0800

    git

--- src/map-sd-1/base-config.js
+++ src/map-sd-1/base-config.js
@@ -13,7 +13,7 @@ const BuildApiHost = {
   spriteHost: `http://47.110.135.245:12808/`, // 精灵图端口
   imageHost: `http://47.110.135.245:12808/`, // 图片地址
   glyphsHost: `http://47.110.135.245:12808/`, // 字体端口
-  apiHost: 'http://localhost:8084/', // api development 环境对应的 ip
+  apiHost: 'http://47.110.135.245:8084/', // api development 环境对应的 ip
   bffHost: `http://47.97.230.212:8091/`, // bff 192.168.43.210:8080   47.110.135.245:12808/   192.168.8.103:8080顾金燕后台  47.110.135.245:10808顾金燕端口
   httpTimeOut: 5000,
 };
