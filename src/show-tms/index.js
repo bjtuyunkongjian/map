cf3f938ef208fcba087a9bb39a06fb8a8058384f
sl204984 <17798520003@163.com>
2020-07-02 10:55:51 +0800

    git

--- src/show-tms/index.js
+++ src/show-tms/index.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react';
 
 export default class index extends Component {
-  state = { visible: true };
+  state = { visible: true, rdColor, bgColor };
 
   render() {
-    const { visible } = this.state;
+    const { rdColor, bgColor } = this.state;
     return (
-      <div className="show-tms" onClick={this._toggleVisible}>
-        {!visible ? '显示 tms 图层' : '隐藏 tms 图层'}
+      <div className="show-tms">
+        中线：
+        <input
+          value={rdColor}
+          type="text"
+          maxLength="6"
+          placeholder="六位十六进制 ffffff"
+          onChange={this._changeRdColor}
+        />
+        底色：
+        <input
+          value={bgColor}
+          type="text"
+          placeholder="六位十六进制 ffffff"
+          maxLength="6"
+          onChange={this._changeBgColor}
+        />
       </div>
     );
   }
 
-  _toggleVisible = () => {
-    const { visible } = this.state;
-    if (visible) {
-      // "visible", "none"
-      _MAP_.setLayoutProperty('guodao_bg', 'visibility', 'none');
-      _MAP_.setLayoutProperty('guodao', 'visibility', 'none');
-    } else {
-      _MAP_.setLayoutProperty('guodao_bg', 'visibility', 'visible');
-      _MAP_.setLayoutProperty('guodao', 'visibility', 'visible');
+  _changeRdColor = (e) => {
+    const color = e.target.value;
+    this.setState({ rdColor: color });
+    if (!reg.test(color)) return;
+    for (let item of roads) {
+      _MAP_.setPaintProperty(`siwei_${item}`, 'line-color', '#' + color);
     }
-    this.setState({ visible: !visible });
+  };
+
+  _changeBgColor = (e) => {
+    const color = e.target.value;
+    this.setState({ bgColor: color });
+    if (!reg.test(color)) return;
+    for (let item of roads) {
+      _MAP_.setPaintProperty(`siwei_${item}_bg`, 'line-color', '#' + color);
+    }
   };
 }
+const reg = /^([a-f]|[A-F]|\d){6}$/;
+
+const rdColor = 'ffffff';
+const bgColor = 'aaaaaa';
+
+const roads = [
+  'dushigaosulu',
+  'gaosugonglu',
+  'guodao',
+  'jiujilu',
+  'qitadaolu',
+  'shengdao',
+  'xiandao',
+  'xiangzhendaolu',
+];
